5bbb1a76e00b7c531b899124f6bf61cd0875ae15
drglasdou <dr.glasdou@gmail.com>
2024-01-30 12:06:36 -0600

    change: employee response in employee controller

--- HRM.Application/UseCases/Employees/Queries/GetEmployeesQuery.cs
+++ HRM.Application/UseCases/Employees/Queries/GetEmployeesQuery.cs
@@ -4,7 +4,8 @@ using MediatR;
 
 namespace HRM.Application.UseCases.Employees.Queries;
 
-public class GetEmployeesQuery(PaginationParams pagination) : IRequest<IEnumerable<EmployeeDto>>
+public class GetEmployeesQuery(PaginationParams pagination, bool isAdmin = false) : IRequest<IEnumerable<EmployeeDto>>
 {
     public PaginationParams Pagination { get; set; } = pagination;
+    public bool IsAdmin { get; set; } = isAdmin;
 }
